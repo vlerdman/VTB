610a7afe1416c80eb2b43dc65d65edf401ec02bc
vlerdman <erdman.vovaa@gmail.com>
2021-10-10 12:05:03 +0300

    My commit2

---
+++ vtb2/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+export default function App() {
+  return (
+    <WebView 
+        source={{ uri: 'http://qwert7yk.beget.tech/test.html'}}
+        />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
